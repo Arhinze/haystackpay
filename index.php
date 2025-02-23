37055877b14b8ad9d190121304fa59a0f8545e87
Arinze <francisokoye48@gmail.com>
2025-02-23 05:52:51 +0100

    making site finer . . finetuning padding, etc

--- index.php
+++ index.php
@@ -71,6 +71,10 @@ if(isset($_POST["referred_accounts"])){
             padding:30px 45px;
         }
 
+        .body{
+            padding:30px;
+        }
+
         textarea{
             width:95%;
             height:150px;
@@ -95,8 +99,10 @@ if(isset($_POST["referred_accounts"])){
     </style>
 </head>
 <body>
+<body>
     <h1 style = "text-align:center"><i class="fa fa-linkedin"></i> Linkedin Rentals</h1>
     <div>
+        <div style="text-align:center">This program is created for managers to know if their tenants' accounts is still active on WANL(Melissa's company).</div>
         <b>Input all the emails you wish to seek for, one on each line.</b>
     </div>
     <form method = "post" action = "">
@@ -120,5 +126,6 @@ if(isset($_POST["referred_accounts"])){
             }
         }    
     ?>
+</body>
 </body>
 </html>
