2a4a4b6a5a655772afe0e7577dff974cc13cc116
Arinze <francisokoye48@gmail.com>
2025-02-24 07:27:44 +0100

    testing out allowing users to input numbers

--- index.php
+++ index.php
@@ -29,11 +29,14 @@ $all_active_accounts = array_map('trim', $all_active_accounts);
 $managers_referrals = [];
 $output = [];
 $isset_of_ref = false;
+$all_referred_accounts = "";
 
 if(isset($_POST["referred_accounts"])){
     $isset_of_ref = true;
+    $all_referred_accounts = $_POST["referred_accounts"];
 
-    $managers_referrals = trim(htmlentities($_POST["referred_accounts"]));
+    $managers_referrals = trim(htmlentities($all_referred_accounts));
+    $managers_referrals = preg_replace("/[0-9]*.|[0-9]*)|[0-9]*.)/", "", $all_referred_accounts);
     //echo "<h2>managers referrals: </h2>". $managers_referrals;
     $managers_referrals_arr = explode("\n", $managers_referrals);
     $managers_referrals_arr = array_map('strtolower', $managers_referrals_arr);
@@ -103,7 +106,9 @@ if(isset($_POST["referred_accounts"])){
     <h1 style = "text-align:center"><i class="fa fa-linkedin"></i> Linkedin Rentals</h1>
     <div>
         <div style="text-align:center;margin-bottom:24px">
-            This program is created for managers to know if their tenants' accounts are still active on WANL(Melissa's company).
+            This program is created for managers to know if their tenants' accounts are still active on WANL(Melissa's company). <br /><br />
+
+            Accepted numbering formats are: 1. || 1) and 1.)
         </div>
         <div style="text-align:center;font-weight:bold;font-size:18px">
             Input all the emails you wish to seek for.<br />
@@ -115,6 +120,7 @@ if(isset($_POST["referred_accounts"])){
         <br /><button class="long-action-button" type = "submit"><i class="fa fa-flash"></i> Submit</button><br /><br />
     </form>
 
+    <div style="margin:24px 15px">
     <?php
         if($isset_of_ref) {
             echo "<h2>This Manager's Referals for the week with active accounts are:</h2>";
@@ -131,6 +137,7 @@ if(isset($_POST["referred_accounts"])){
             }
         }    
     ?>
+    </div>
 </body>
 </html>
 
