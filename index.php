5e39c5f99171b137df9cc6bdc3209d07452b8cc8
Arinze <francisokoye48@gmail.com>
2025-02-24 07:33:11 +0100

    testing out allowing users to input numbers

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ if(isset($_POST["referred_accounts"])){
     $all_referred_accounts = $_POST["referred_accounts"];
 
     $managers_referrals = trim(htmlentities($all_referred_accounts));
-    $managers_referrals = preg_replace("/[0-9].|[0-9])|[0-9].)/", "", $all_referred_accounts);
+    $managers_referrals = preg_replace("/[0-9].|[0-9]\)|[0-9].\)/", "", $all_referred_accounts);
     //echo "<h2>managers referrals: </h2>". $managers_referrals;
     $managers_referrals_arr = explode("\n", $managers_referrals);
     $managers_referrals_arr = array_map('strtolower', $managers_referrals_arr);
