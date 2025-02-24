5cd2f521775096c5bb2e7abe63e65ee8399db39f
Arinze <francisokoye48@gmail.com>
2025-02-24 21:17:38 +0100

    adding the view-file feature to highlight mangers' active users

--- melissa.php
+++ melissa.php
@@ -24,7 +24,6 @@ if(isset($_POST["referred_accounts"])){
     $managers_referrals_arr = array_map('trim', $managers_referrals_arr);
     $managers_referrals_arr = array_unique($managers_referrals_arr);
     //echo "<h2>managers referrals array:</h2>"; print_r($managers_referrals_arr);
-    $json_all_mans = json_encode($managers_referrals_arr);
     
     foreach($managers_referrals_arr as $managers_ref) { //if(!in_array()) could be used here in place of array_unique
         if(in_array($managers_ref, $all_active_accounts)){
@@ -120,7 +119,7 @@ if(isset($_POST["referred_accounts"])){
             }
     ?>
         <form method="post" action="/view-file">
-            <input type="hidden" name="managers_accounts" value="<?=$json_all_mans?>"/>
+            <input type="hidden" name="managers_accounts" value="<?=json_encode($managers_referrals_arr)?>"/>
             <!-- $managers_referrals_arr-->
             
             <button class="long-action-button" style="background-color:blue;color:#fff" type="submit"> <i class="fa fa-file"></i> View on File >> </button>
