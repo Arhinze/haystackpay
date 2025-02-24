ed1e0b4171943c30a2291e272dc8cba1839e2424
Arinze <francisokoye48@gmail.com>
2025-02-24 21:23:37 +0100

    adding the view-file feature to highlight mangers' active users

--- melissa.php
+++ melissa.php
@@ -119,8 +119,8 @@ if(isset($_POST["referred_accounts"])){
             }
     ?>
         <form method="post" action="/view-file">
-            json_encode(htmlentities($managers_referrals_arr))
-            <input type="hidden" name="managers_accounts" value="<>"/>
+            <?php $js_e = htmlentities(json_encode($managers_referrals_arr)); ?>
+            <input type="hidden" name="managers_accounts" value="<?=$jse?>"/>
             <!-- $managers_referrals_arr-->
             
             <button class="long-action-button" style="background-color:blue;color:#fff" type="submit"> <i class="fa fa-file"></i> View on File >> </button>
