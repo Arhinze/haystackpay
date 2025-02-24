06462e59c17f063974204dab35d21c4dafae9c35
Arinze <francisokoye48@gmail.com>
2025-02-24 22:11:30 +0100

    adding the view-file feature to highlight mangers' active users

--- index.php
+++ index.php
@@ -1,7 +1,21 @@
 <?php
-
+/*
 include_once($_SERVER["DOCUMENT_ROOT"]."/views/Index_Segments.php");
 
 Segments::header();
 
 Segments::footer();
+
+*/
+?>
+
+<br />
+<br />
+<br />
+<br />
+<h1>Haystackpay</h1>
+ ~ coming soooon ~
+<br />
+<br />
+<br />
+<h3><a href="/melissa">WANL(Melissa)</a></h3>
