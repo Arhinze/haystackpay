08b6a07c2b2c74ff6f7bd830f6444af3aa37329d
Arinze <francisokoye48@gmail.com>
2025-02-24 09:39:13 +0100

    testing out allowing users to input numbers

--- index.php
+++ index.php
@@ -108,7 +108,7 @@ if(isset($_POST["referred_accounts"])){
         <div style="text-align:center;margin-bottom:24px">
             This program is created for managers to know if their tenants' accounts are still active on WANL(Melissa's company). <br /><br />
 
-            <div style="color:#ff9100">Accepted numbering formats are: 1. , 1) or 1.)</div>
+            <div style="color:#ff9100;font-weight:bold">Accepted numbering formats are: 1. , 1) or 1.)</div>
         </div>
         <div style="text-align:center;font-weight:bold;font-size:18px">
             Input all the emails you wish to seek for.<br />
@@ -123,7 +123,7 @@ if(isset($_POST["referred_accounts"])){
     <div style="margin:24px 15px">
     <?php
         if($isset_of_ref) {
-            echo "<h2>This Manager's Referals for the week with active accounts are:</h2>";
+            echo "<h2 style='text-align:center'>This Manager's Referals for the week with active accounts are:</h2>";
             //print_r($managers_referrals_arr);
 
             if(count($output) == 0) {
