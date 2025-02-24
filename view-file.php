9584b53ad8a29d75f245db3305af9c4e231e2c6f
Arinze <francisokoye48@gmail.com>
2025-02-24 22:17:15 +0100

    adding the view-file feature to highlight mangers' active users

--- view-file.php
+++ view-file.php
@@ -79,14 +79,15 @@ $all_active_accounts = array_map('trim', $all_active_accounts);
                 //echo "managers_ref_arr: "; print_r($managers_referrals_arr); echo "<br />";
 
                 foreach($all_active_accounts as $all_act_acct) {
-                    $i++;
+                    $i++; $man_acct_no = 0;
                     if(in_array($all_act_acct, $managers_referrals_arr)) {
+                        $man_acct_no++;
                         echo "<b style = 'color:green'>$i.) $all_act_acct</b> <i class='fa fa-check-circle'></i> <br />";
                     } else {
                         echo "<span style='color:#888'>", $i, ".) ", $all_act_acct, "</span><br />";
                     }
                 }
-            echo "<br /><br /><b>Managers Total Active Referrals:</b> ", count($managers_referrals_arr);
+            echo "<br /><br /><b>Managers Total Active Referrals:</b> ", count($man_acct_no);
             } else {
                 foreach($all_active_accounts as $all_act_acct) {
                     $i++;
