330d92fbcc03f0c71f3c2042512f7543c9dbc985
Arinze <francisokoye48@gmail.com>
2025-03-01 11:38:31 +0100

    removing melissa page from public access and starting earnest work on haystackpay

--- index.php
+++ index.php
@@ -9,16 +9,20 @@ Segments::footer();
 */
 ?>
 
-<br />
-<br />
-<br />
-<br />
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div style="text-align:center">
+        <img src="/static/images/logo.png" style="width:120px;height:120px;border-radius:100%"/>
+        <br />
+        <h1>Haystackpay</h1>
+         ~ coming soooon ~
+    </div>
+</body>
+</html>
 
-<img src="/static/images/logo.png" style="width:120px;height:120px;border-radius:100%"/>
-<br />
-<h1>Haystackpay</h1>
- ~ coming soooon ~
-<br />
-<br />
-<br />
-<h3><a href="/melissa">WANL(Melissa)</a></h3>
