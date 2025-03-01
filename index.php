71890ccadfe62684dd58fffdad2e644660dac063
Arinze <francisokoye48@gmail.com>
2025-03-01 11:47:56 +0100

    removing melissa page from public access and starting earnest work on haystackpay

--- index.php
+++ index.php
@@ -14,13 +14,13 @@ Segments::footer();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>HAYSTACKPAY</title>
 </head>
 <body>
-    <div style="text-align:center">
+    <div style="text-align:center; margin-top:30px">
         <img src="/static/images/logo.png" style="width:120px;height:120px;border-radius:100%"/>
         <br />
-        <h1>Haystackpay</h1>
+        <h1>HAYSTACKPAY</h1>
          ~ coming soooon ~
     </div>
 </body>
