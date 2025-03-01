719fdaa4bb9ccff0d3a1f97f48fa41c864931ea5
Arinze <francisokoye48@gmail.com>
2025-03-01 11:52:07 +0100

    removing melissa page from public access and starting earnest work on haystackpay

--- melissa.php
+++ melissa.php
@@ -6,9 +6,9 @@
     <link rel="stylesheet" href="/static/font-awesome-4.7.0/css/font-awesome.min.css"/>
     <title>WANL - Melissa</title>
 </head>
-<body style="text-align:center;background-color:#f8f8f8;">
+<body style="text-align:center;background-color:#f8f8f8;padding:24px 12px">
     <h1>Customized software for WANL ~ Melissa:</h1>
-    <h3>
+    <h3 style="margin-top:21px">
         Please <a href="https://wa.link/94f4vk"><i class="fa fa-whatsapp"></i> contact the Software Developer</a> to gain access to this service.
     </h3>
 </body>
