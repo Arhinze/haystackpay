12fe9fa1515720005b8e30942c0e4ac91f817def
Arinze <francisokoye48@gmail.com>
2025-03-01 13:39:48 +0100

    officially starting off work in haystack

--- views/Index_Segments.php
+++ views/Index_Segments.php
@@ -83,7 +83,7 @@ class Index_Segments{
                     <div style="color:#ff9100;font-weight:bold">Accepted numbering formats are: 1. , 1) or 1.)</div>
 
                     <form method = "post" action = "">
-                        <textarea name="referred_accounts" placeholder="Enter the email addresses to get started"></textarea>
+                        <textarea name="referred_accounts" class="index_textarea" placeholder="Enter the email addresses to get started"></textarea>
                         <br /><button class="long-action-button" type = "submit"><i class="fa fa-flash"></i> Submit</button><br /><br />
                     </form>
                 </div>
