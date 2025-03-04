6bb5a9b9abce6f5cad74a45a220fee63113986a7
Arinze <francisokoye48@gmail.com>
2025-03-04 14:25:13 +0100

    working on confirm-transaction page

--- confirm-transaction.php
+++ confirm-transaction.php
@@ -12,19 +12,21 @@ if($data) {//that means user is logged in:
         //disburse funds to all emails
         $all_valid_emails = explode("#", $mails_to_disburse_to);
         foreach($all_valid_emails as $ave) {
-            if ($hstkp_transactions->user_exists($ave)) {
-                $hstkp_transactions->deposit($hstkp_transactions->user_exists($ave)->user_id, $amt_for_each_person, "Received from: ".$data->username); 
-                // - mail() $ave
-            } else {
-                $p_stmt = $pdo->prepare("INSERT INTO haystack_users(real_name, username, user_email, `password`,airdrop_status,twitter_username,avax_wallet_address,aguat_wallet_address,referred_by,entry_date,mining_status,mining_start_time) VALUES(?, ?,?, ?, ?, ?,?,?,?,?,?,?)");
-
-                $p_stmt->execute(["Dummy Real Name", $ave, $ave, "123pc","not_participated"," "," "," ",$data->username,date("Y-m-d H:i:s", time()),date("Y-m-d H:i:s", time()),"inactive"]);
-
-                if ($hstkp_transactions->user_exists($ave)) {//now user exists if previously does not exist . .
+            if(!empty($ave)) {//so as not to insert empty string
+                if ($hstkp_transactions->user_exists($ave)) {
                     $hstkp_transactions->deposit($hstkp_transactions->user_exists($ave)->user_id, $amt_for_each_person, "Received from: ".$data->username); 
                     // - mail() $ave
+                } else {
+                    $p_stmt = $pdo->prepare("INSERT INTO haystack_users(real_name, username, user_email, `password`,airdrop_status,twitter_username,avax_wallet_address,aguat_wallet_address,referred_by,entry_date,mining_status,mining_start_time) VALUES(?, ?,?, ?, ?, ?,?,?,?,?,?,?)");
+    
+                    $p_stmt->execute(["Dummy Real Name", $ave, $ave, "123pc","not_participated"," "," "," ",$data->username,date("Y-m-d H:i:s", time()),date("Y-m-d H:i:s", time()),"inactive"]);
+    
+                    if ($hstkp_transactions->user_exists($ave)) {//now user exists if previously does not exist . .
+                        $hstkp_transactions->deposit($hstkp_transactions->user_exists($ave)->user_id, $amt_for_each_person, "Received from: ".$data->username); 
+                        // - mail() $ave
+                    }
                 }
-            }
+            } //end of if(!empty($ave))
         }
     }
 
