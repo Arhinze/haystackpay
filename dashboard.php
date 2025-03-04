d41d1f68c9a3500699437a8bd35ab67a507a9560
Arinze <francisokoye48@gmail.com>
2025-03-04 10:45:06 +0100

    displaying transactions on dashboard

--- dashboard.php
+++ dashboard.php
@@ -2,38 +2,42 @@
 
 include_once($_SERVER["DOCUMENT_ROOT"]."/views/Dashboard_Segments.php");
 
-    if($data){
-        // that means user is logged in:
-
-        //cookie variables:
-        $form_user_id = $_COOKIE["username_or_email"];
-        $form_password = $_COOKIE["password"];
-
-        //~check if user made a new transaction:
-        if(isset($_GET["new_transaction"])){
-            $trans_id = (int)htmlentities($_GET["new_transaction"]);
-            if ((time() - strtotime($hstkp_transactions->tr_time($trans_id))) < (1*60)){//if transaction is not more than 1 min old
-                echo "<div class='pop_up'>Transaction successful. <span style='float:right;position:absolute;top:6px;right:6px'><i class='fa fa-times' onclick='close_pop_up()'></i></span></div>";
-            } else { //to avoid excessive access to mysql database ~ I don't know if this is necessary though
-                header("location:/dashboard");
-            }
+if($data){// that means user is logged in:
+    // ~ check if user made a new transaction:
+    if(isset($_GET["new_transaction"])) {
+        $trans_id = (int)htmlentities($_GET["new_transaction"]);
+        if ((time() - strtotime($hstkp_transactions->tr_time($trans_id))) < (1*60)){//if transaction is not more than 1 min old
+            echo "<div class='pop_up'>Transaction successful. <span style='float:right;position:absolute;top:6px;right:6px'><i class='fa fa-times' onclick='close_pop_up()'></i></span></div>";
+        } else { //to avoid excessive access to mysql database ~ I don't know if this is necessary though
+            header("location:/dashboard");
         }
+    }
      
-        //display header:
-        //Dashboard_Segments::header($site_name = SITE_NAME_SHORT, $site_url = SITE_URL, $Hi_user = "");
-        Dashboard_Segments::header($site_name = SITE_NAME_SHORT, $site_url = SITE_URL, $Hi_user = $data->username,$title=SITE_NAME." - Dashboard");
-        
-        
+    //display header:
+    //Dashboard_Segments::header($site_name = SITE_NAME_SHORT, $site_url = SITE_URL, $Hi_user = "");
+    Dashboard_Segments::header($site_name = SITE_NAME_SHORT, $site_url = SITE_URL, $Hi_user = $data->username,$title=SITE_NAME." - Dashboard");  
 ?>
 
 <div style="margin:180px 15px 90px 15px">
     <!-- This is going off soon -->
+    <!--
     <div style="text-align:center">
         <p>Sorry, we're still working on this project.</p>
         <p>We believe this little tour has been able to give you an idea of what the project is all about</p>
         <p>For further enquiries, kindly <b><a href="https://wa.link/hgzcrj" style="color:#ff9100">click here <i class="fa fa-whatsapp"></i></a></b> to contact the software developer, or call on: <b>+2348106961530</b>.</p>
     </div>
+    -->
     <!--End of: This is going off soon -->
+
+    <div class="dashboard">
+        <h2>Your Transactions:</h2>
+        <?php
+            foreach($hstkp_transactions->all_trs($data->user_id) as $all_h_tr) {
+                $tr_color = $all_h_tr->tr_type = "inflow" ? "green" : "red";
+                echo "<div style='color:$tr_color;font-size:15px'><i class='fa fa-circle'></i>", $all_h_tr->tr_type, " - $all_h_tr->tr_amount - $all_h_tr->tr_time <i class='fa fa-angle-down'><i/></div>";
+            }
+        ?>
+    </div>
 
     <!-- Referral Link section starts -->
     <div style="padding:12px">
@@ -53,7 +57,7 @@ include_once($_SERVER["DOCUMENT_ROOT"]."/views/Dashboard_Segments.php");
 
 <?php
     Dashboard_Segments::dashboard_footer(); 
-    } else { /*end of if($data) for cookie name and pass .. else means user is not logged in*/
-        header("location:/login");
-    } 
+} else { /*end of if($data) for cookie name and pass .. else means user is not logged in*/
+    header("location:/login");
+} 
 ?>
