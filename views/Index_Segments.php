f7697dc78bf7783a68dac3cb5a9e9d0f0a07da6d
Arinze <francisokoye48@gmail.com>
2025-03-04 02:01:41 +0100

    adding small logo beside site name

--- views/Index_Segments.php
+++ views/Index_Segments.php
@@ -40,7 +40,7 @@ class Index_Segments{
               
         </head>
         <body>
-            <div class="headers" style="display:flex;padding:6px 3px;background-color:blue">  
+            <div class="headers" style="display:flex;padding:6px 3px">  
                 <div style="float:left;margin-top:-13px"><img src="/static/images/logo_rbg.png" class="site_name_logo"/></div>
                 <div style="float:left;margin-top:-19px">
                     <h3 class="site_name"><a href="/">HAYSTACK<span style="color:#ff9100">PAY</span><!--$site_name--></a></h3>
