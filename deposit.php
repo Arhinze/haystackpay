d65547939eafb67087946b2009c5fe1f98a17fb2
Arinze <francisokoye48@gmail.com>
2025-03-04 15:10:39 +0100

    adding deposit page

--- deposit.php
+++ deposit.php
@@ -10,8 +10,8 @@ if($data){// that means user is logged in:
 
 
 <div style="margin:180px 15px 90px 15px">
-    <form method="post" action="" style="background-color:#fff;padding:15px 6px;border-radius:9px">
-        <input type="number" class="input" placeholder="N100 - N5,000,000" style="border:0;border-bottom:1px solid #fff;margin:18px 3px;padding:12px"/>
+    <form method="post" action="" style="background-color:#fff;padding:15px 6px;border-radius:9px;border:1px solid #ff9100">
+        <input type="number" class="input" placeholder="N100 - N5,000,000" style="border:0;border-bottom:1px solid #888;margin:18px 3px;padding:12px"/>
 
         <button type="submit" class="long-action-button" style="background-color:green;color:#fff">
             Confirm
