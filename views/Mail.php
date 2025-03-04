ad322571f43dfe49cee77e9cb354cb675b13568c
Arinze <francisokoye48@gmail.com>
2025-03-04 21:05:14 +0100

    adding paystack to haystackpay ~ solving duplicate_transaction_problem

--- views/Mail.php
+++ views/Mail.php
@@ -12,115 +12,53 @@ if($data) {
 if(isset($_POST["deposit_amount"])) {
     $dep_amount = htmlentities($_POST["deposit_amount"]);
 }
-      
-$attempted_deposit_message = <<<HTML
-    <html><head><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Trirong|Arimo"/><link rel="stylesheet" href="$site_url/static/font-awesome-4.7.0/css/font-awesome.min.css"/></head>
-    <body style ="font-family:Trirong;">
-        <div style="position:relative">
-            <img src="$site_url/static/images/logo_rbg.png" style="margin-left:36%;margin-right:36%;width:25%;position:absolute"/>
-        </div>
-        <h2 style="color:#00008b;font-family:Arimo;text-align:center">$site_name</h2>
-            <p  style ="font-family:Trirong;">
-                Hello Sir, a user with username: $username attempted to deposit N$dep_amount in his dashboard.
-            </p>
-    </body>
-    </html>
+
+$mail_body_top = <<<HTML
+    <html>
+        <head>
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Trirong|Arimo"/><link rel="stylesheet" href="$site_url/static/font-awesome-4.7.0/css/font-awesome.min.css"/>
+        </head>
+        <body style ="font-family:Trirong;">
+            <div style="position:relative">
+                <img src="$site_url/static/images/logo_rbg.png" style="margin-left:36%;margin-right:36%;width:25%;position:absolute"/>
+            </div>
+            <h2 style="color:#00008b;font-family:Arimo;text-align:center">$site_name</h2>
 HTML;
 
+$mail_body_bottom = "</body></html>";
 
-
-
-
-
-
-
-//mail  Admin, Sales-Rep:
-//$new_order_name = $_GET["name"];
-//$new_order_phone = $_GET["phone"];
-//$new_order_qty = $_GET["qty"]; 
-//$product_name = $_GET["product"];
-//$customer_mail = $_GET["mail"];
-
-$new_order_name = "";
-$new_order_phone = "";
-$new_order_qty = ""; 
-$product_name = '';
-$customer_mail = "";
+$attempted_deposit_message = <<<HTML
+    $mail_body_top
+        <p  style ="font-family:Trirong;">
+            Hello Sir, a user with username: <b>$username</b> and email address: <b>$user_mail</b> is attempting to deposit <b>N$dep_amount</b> in his/her dashboard.
+        </p>
+    $mail_body_bottom
+HTML;
 
 $admin_successful_deposit_message = <<<HTML
-    <html>
-    <head>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Trirong|Arimo"/>
-        <link rel="stylesheet" href="$site_url/static/font-awesome-4.7.0/css/font-awesome.min.css"/>
-    </head>
-    <body style ="font-family:Trirong;">
-        <div style="position:relative">
-            <img src="$site_url/static/images/logo.png" style="margin-left:36%;margin-right:36%;width:25%;position:absolute"/>
-        </div>
-        
-        <h2 style="color:#00008b;font-family:Arimo;text-align:center">$site_name</h2>
-            <p  style ="font-family:Trirong;">
-            
-            Hello Sir/Ma, you just received a new Paid order for $product_name from: 
-            <b>$new_order_name</b> with the phone Number: <b>$new_order_phone</b>. 
-            </p>
-            
-            <p>
-                <h3>Paid Order details:</h3>
-                <div>
-                    <b>Name:</b> $new_order_name
-                </div> <br />
-                
-                <div>
-                    <b>Phone Number:</b> $new_order_phone
-                </div> <br />
-                     
-                <div>
-                    <b>Quantity:</b> $new_order_qty
-                </div> <br />
-                
-                <div>
-                    <b>Message:</b> 
-                    <p>Kindly find message in the previous mail sent on attempted payment</p>
-                </div>
-            </p>
-    </body>
-    </html>
+    $mail_body_top
+        <p>Hello Sir/Ma, A user successfully desposited: $dep_amount.</p>
+        <p>Username: <b>$username</b>, Email: <b>$user_mail</b>.</p>
+    $mail_body_bottom
 HTML;
 
-
 $user_successful_deposit_message = <<<HTML
-    <html>
-    <head>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Trirong|Arimo"/>
-        <link rel="stylesheet" href="$site_url/static/font-awesome-4.7.0/css/font-awesome.min.css"/>
-    </head>
-    <body style ="font-family:Trirong;">
-        <div style="position:relative">
-            <img src="$site_url/static/images/logo.png" style="margin-left:36%;margin-right:36%;width:25%;position:absolute"/>
-        </div>
-        
-        <h2 style="color:#00008b;font-family:Arimo;text-align:center">$site_name</h2>
-            <p  style ="font-family:Trirong;">
-            
-            Thank You, <b>$new_order_name</b> for your order of $product_name from: <b>$site_name</b>. 
-            </p>
-            
-            <p>
-                <div>
-                    One of our sales representatives would get in touch with you soon, and you would receive your order in 2 to 3 working days or less.
-                </div> <br />
-                
-                <div>
-                    Kindly feel free to send us a mail or call any of the numbers on our website page if you're in need of further clarifications.
-                </div> <br />
-                     
-                <div>
-                    Thanks a lot once more sir/ma.
-                </div> <br />
-            </p>
-    </body>
-    </html>
+    $mail_body_top
+        <p  style ="font-family:Trirong;">
+            Congratulations, <b>$username</b>your deposit of <b>$dep_amount</b> on <b>$site_name</b> was successful. 
+        </p>
+        <p>
+            <div>
+                Thank you for banking with us. Do well to keep in tough with us as we revolutionize the banking system and banking experience.
+            </div> <br />
+            <div>
+                Kindly feel free to send us a mail or call any of the numbers on our website page if you're in need of any clarifications and keep in touch with us on our various social media platforms.
+            </div> <br />
+            <div>
+                Thanks a lot once more sir/ma.
+            </div> <br />
+        </p>
+    $mail_body_bottom
 HTML;
 
 $sender = "admin@$site_url_short";
