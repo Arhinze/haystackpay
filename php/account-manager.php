4d9f2fbd874ce1c7108947a04a67becdcf1458dc
Arinze <francisokoye48@gmail.com>
2025-03-04 08:16:59 +0100

    created Transactions class to handle all transactions

--- php/account-manager.php
+++ php/account-manager.php
@@ -17,14 +17,56 @@ if((isset($_COOKIE["username_or_email"])) && ((isset($_COOKIE["password"])))){
 // then call 'if data(){ ... }' for all necessary dashboard related page.
 
 
-//Updating status to 'inactive' and adding the balance for all haystack_users after 48 hours:
-$gen_stmt = $pdo->prepare("SELECT * FROM haystack_users WHERE mining_status = ? LIMIT ?, ?");
-$gen_stmt->execute(["active", 0, 500]);
-$gen_data = $gen_stmt->fetchAll(PDO::FETCH_OBJ);
+//Managing all the transactions made by users on haystackpay
+class Transactions {
+    private $pdo;
 
-foreach($gen_data as $gd) {
-    if (strtotime($gd->mining_start_time) <= (time() - (48*60*60))) {
-        $gen_updt = $pdo->prepare("UPDATE haystack_users SET mining_status = ?, total_amount_mined = ? WHERE user_id = ?");
-        $gen_updt->execute(["inactive",$gd->total_amount_mined + 1.00224, $gd->user_id]);
+    public function inject($obj){
+        $this->pdo = $obj;
+    }
+
+    private function tr_alert($message){
+        echo "<div class='invalid' style='background-color:green'>".$message."</div>";
+    }
+
+    public function deposit($user_id,$amt,$from){
+        $dep_stmt = $this->pdo->prepare("INSERT INTO transactions(tr_id, user_id, tr_type, tr_amount, tr_time, tr_from) VALUES (?,?,?,?,?,?)");
+        $dep_stmt->execute([null, $user_id, "inflow", $amt, date("Y-m-d h:ia",time(), $from)]);
+
+        return $this->tr_alert("Deposit made successfully"); 
+    }
+
+    public function withdraw($user_id,$amt,$from){
+        $wit_stmt = $this->pdo->prepare("INSERT INTO transactions(tr_id, user_id, tr_type, tr_amount, tr_time, tr_from) VALUES (?,?,?,?,?,?)");
+        $with_stmt->execute([null, $user_id, "outflow", $amt, date("Y-m-d h:ia",time(), $from)]);
+
+        return $this->tr_alert("Withdrawal made successfully"); 
+    }
+
+    public function current_balance($user_id) {
+        //inflow:
+        $cb_in_stmt = $this->pdo->prepare("SELECT * FROM transactions WHERE user_id = ? AND tr_type = ? LIMIT ?, ?");
+        $cb_in_data = $cb_in_stmt->execute([$data->user_id, "inflow", 0, 1000]);
+
+        $total_cb_in = 0;
+        foreach($cb_in_data as $cb_in) {
+            $total_cb_in += $cb_in;
+        }
+
+        //outflow:
+        $cb_out_stmt = $this->pdo->prepare("SELECT * FROM transactions WHERE user_id = ? AND tr_type = ? LIMIT ?, ?");
+        $cb_out_data = $cb_out_stmt->execute([$data->user_id, "outflow", 0, 1000]);
+
+        $total_cb_out = 0;
+        foreach($cb_out_data as $cb_out) {
+            $total_cb_out += $cb_out;
+        }
+
+        //current balance:
+        $current_balance = $total_cb_in - $total_cb_out;
+        return $current_balance;
     }
 }
+
+$transactions = new Transactions;
+$transactions->inject($pdo);
