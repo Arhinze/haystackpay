8a0c0294628a6e2292c30b88e91e4ef03ef27512
Arinze <francisokoye48@gmail.com>
2025-03-05 12:46:59 +0100

    adding dashboard_body_top()

--- views/Dashboard_Segments.php
+++ views/Dashboard_Segments.php
@@ -104,7 +104,7 @@ HTML;
                     <div class="dashboard_top_div"><i class="fa fa-bank"></i></div>
                     <div class="dashboard_top_text">Withdraw to bank</div>
                 </div>
-                <div class="dashboard_top_section" onclick="pop_up('This feature is coming soon')"><span id="pop_up"></span>
+                <div class="dashboard_top_section" onclick="pop_up('This feature is coming soon')"><span id="pop_up" class="pop_up"></span>
                     <div class="dashboard_top_div"><i class="fa fa-exchange"></i></div>
                     <div class="dashboard_top_text">Convert</div>
                 </div>
