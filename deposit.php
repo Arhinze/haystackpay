83935dfbded570d69c2d0aa5381233cf2f7f7e7c
Arinze <francisokoye48@gmail.com>
2025-03-05 11:14:54 +0100

    creating withdraw page

--- deposit.php
+++ deposit.php
@@ -89,7 +89,7 @@ if($data) {// that means user is logged in:
 
 <div style="margin:180px 15px 90px 15px">
     <h2>Add Money</h2>
-    <form method="post" action="" style="background-color:#fff;padding:12px;border-radius:9px;border:1px solid #ff9100">
+    <form method="post" action="" class="dashboard_form">
         <input type="number" class="input" name="deposit_amount" placeholder=" N100 - N5,000,000" style="border:1px solid #888;margin:18px 3px;height:42px;width:96%" required/>
 
         <button type="submit" class="long-action-button" style="background-color:green;color:#fff;width:96%">
