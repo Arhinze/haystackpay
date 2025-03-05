c2b95552c87c77907ecf36013bea8157b6b343a5
Arinze <francisokoye48@gmail.com>
2025-03-05 08:14:56 +0100

    adding paystack to haystackpay ~ solving duplicate_transaction_problem

--- ajax/ajax_cb.php
+++ ajax/ajax_cb.php
@@ -12,6 +12,6 @@ if($data) { // ~ user is logged in
     $current_balance = $hstkp_transactions->current_balance($data->user_id);
     echo "N",$current_balance;
 } else { // ~ user is not logged in
-    echo "Nnot available, kindly log in.";
+    echo "not available, kindly log in.";
 }
 ?>
