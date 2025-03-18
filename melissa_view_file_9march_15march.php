b8dafcaacf1ebd8c211f15b5a81797aa0352fa11
Arinze <francisokoye48@gmail.com>
2025-03-19 00:28:08 +0100

    melissa files

--- melissa_view_file_9march_15march.php
+++ melissa_view_file_9march_15march.php
@@ -1,6 +1,6 @@
 <?php
 
-$linkedin_file = trim(htmlentities(file_get_contents($_SERVER["DOCUMENT_ROOT"]."/static/files/linkedin_9march_15march.xls")));
+$linkedin_file = trim(htmlentities(file_get_contents($_SERVER["DOCUMENT_ROOT"]."/static/files/linkedin_9march_15march.csv")));
 
 $linkedin_file = str_replace("+", "", $linkedin_file);
 $all_active_accounts = explode("\n", $linkedin_file);
