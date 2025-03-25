26e43a9c0f0d45f1de627ade731ba657ff56fb7e
Arinze <francisokoye48@gmail.com>
2025-03-25 08:04:25 +0100

    updating melissa page.. making it easier to use

--- melissa/display_message.php
+++ melissa/display_message.php
@@ -47,7 +47,7 @@
 </head>
 <body>
     <h1 style = "text-align:center"><i class="fa fa-linkedin"></i> Linkedin Rentals</h1>
-    <div>
+    <div style="margin:24px 12px">
         <div style="text-align:center;margin-bottom:24px">
             Please specify a time range you wish to search or view. <br />
             The time(date) range for this program usually spans from sunday to saturday.
@@ -56,10 +56,10 @@
 
         <h3 style="color:#ff9100;font-weight:bold;text-align:center"> Some Samples from 2025: </h3>
 
-        <div style="text-align:center;font-weight:bold;font-size:18px;color:#000">
-           <b><a href="/melissa/active_accounts/2march_8march">haystackpay.com/melissa/active_accounts/2march_8march</a></b><br />
-           <b><a href="/melissa/active_accounts/9march_15march">haystackpay.com/melissa/active_accounts/9march_15march</a></b><br />
-           <b><a href="/melissa/active_accounts/16march_22march">haystackpay.com/melissa/active_accounts/16march_22march</a></b><br />
+        <div style="text-align:center;font-weight:bold;font-size:18px">
+           <b><a href="/melissa/active_accounts/2march_8march" style="color:#000">haystackpay.com/melissa/active_accounts/2march_8march</a></b><br />
+           <b><a href="/melissa/active_accounts/9march_15march" style="color:#000">haystackpay.com/melissa/active_accounts/9march_15march</a></b><br />
+           <b><a href="/melissa/active_accounts/16march_22march" style="color:#000">haystackpay.com/melissa/active_accounts/16march_22march</a></b><br />
         </div>
     </div>
 </body>
