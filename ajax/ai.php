2122ccb8a7426470ae6f662ac303c08ac71650f5
Arinze <francisokoye48@gmail.com>
2025-11-21 08:27:17 +0100

    trying to add AI voice recognition

--- ajax/ai.php
+++ ajax/ai.php
@@ -7,11 +7,17 @@ if (!isset($_GET['message'])) {
 $style= "position:fixed;z-index:5;background-color:#fff;border-radius:6px;border:1px solid #2b8eeb;height:150px;width:70%;text-align:center;font-weight:bold;top:25%;left:5%";
 
 if ($message = "send 5000 to Francis"){
-    echo "<div style='position:fixed;z-index:5;background-color:#fff;border-radius:6px;border:1px solid #2b8eeb;height:150px;width:70%;text-align:center;font-weight:bold;top:25%;left:5%'>Do you wish to send 5000 Naira to 0095000000 Access Bank - Francis Arinze Okoye</div>";
+    echo "<div style='position:fixed;z-index:5;background-color:#fff;border-radius:6px;border:1px solid #2b8eeb;height:150px;width:70%;text-align:center;font-weight:bold;top:25%;left:5%'>Do you wish to send 5000 Naira to 0095000000 Access Bank - Francis Arinze Okoye <div class='button'>Yes, Proceed<i class='fa fa-angle-right'></i></div></div>";
 } else if ($message = "send 5000 to Frances"){
-    echo "<div style='position:fixed;z-index:5;background-color:#fff;border-radius:6px;border:1px solid #2b8eeb;height:150px;width:70%;text-align:center;font-weight:bold;top:25%;left:5%'>Do you wish to send 5000 Naira to 0095000000 Access Bank - Francis Arinze Okoye</div>";
+    echo "<div style='position:fixed;z-index:5;background-color:#fff;border-radius:6px;border:1px solid #2b8eeb;height:150px;width:70%;text-align:center;font-weight:bold;top:25%;left:5%'>Do you wish to send 5000 Naira to 0099000000 Access Bank - Frances Achalugo</div>";
+} else if ($message = "send 6000 to Cynthia"){
+    echo "<div style='position:fixed;z-index:5;background-color:#fff;border-radius:6px;border:1px solid #2b8eeb;height:150px;width:70%;text-align:center;font-weight:bold;top:25%;left:5%'>Do you wish to send 6000 Naira to 2218000000 UBA Bank - Cynthia Chidinma Ochuba</div>";
+} else if ($message = "send 10,000 to Pascal"){
+    echo "<div style='position:fixed;z-index:5;background-color:#fff;border-radius:6px;border:1px solid #2b8eeb;height:150px;width:70%;text-align:center;font-weight:bold;top:25%;left:5%'>Do you wish to send 10,000 Naira to 2291300000 Sterling Bank - Pascal Nnamdi Okafor</div>";
+} else if ($message = "send 3000 to Samuel"){
+    echo "<div style='position:fixed;z-index:5;background-color:#fff;border-radius:6px;border:1px solid #2b8eeb;height:150px;width:70%;text-align:center;font-weight:bold;top:25%;left:5%'>Do you wish to send 3000 Naira to 2254000000 Zenith Bank - Samuel Jaruma Ibrahim</div>";
 } else {
-    echo "<div style='position:fixed;z-index:5;background-color:#fff;border-radius:6px;border:1px solid #2b8eeb;height:150px;width:70%;text-align:center;font-weight:bold;top:25%;left:5%'>Sorry, this user is not a saved beneficiary</div>";
+    echo "<div style='position:fixed;z-index:5;background-color:#fff;border-radius:6px;border:1px solid #2b8eeb;height:150px;width:70%;text-align:center;font-weight:bold;top:25%;left:5%'>Sorry no response gotten/ this user is not a saved beneficiary</div>";
 }
 
 ?>
