5690f92e9b9150ef6c6a2b34c147d12f26548e0d
Arinze <francisokoye48@gmail.com>
2025-11-21 08:04:05 +0100

    adding AI voice recognition

--- ai.php
+++ ai.php
@@ -4,12 +4,14 @@ if (!isset($_GET['message'])) {
     $message = htmlentities($_POST["message"]);
 }
 
+$style= "position:fixed;z-index:5;background-color:#fff;border-radius:6px;border:1px solid #2b8eeb;height:150px;width:70%;text-align:center;font-weight:bold;top:30%;left:5%";
+
 if ($message = "send 5000 to Francis"){
-    return "Do you wish to send 5000 Naira to 0095000000 Access Bank - Francis Arinze Okoye";
+    return "<div style=$style>Do you wish to send 5000 Naira to 0095000000 Access Bank - Francis Arinze Okoye</div>";
 } else if ($message = "send 5000 to Frances"){
-    return "Do you wish to send 5000 Naira to 0095000000 Access Bank - Francis Arinze Okoye";
+    return "<div style=$style>Do you wish to send 5000 Naira to 0095000000 Access Bank - Francis Arinze Okoye</div>";
 } else {
-    return "Sorry, this user is not a saved beneficiary";
+    return "<div style=$style>Sorry, this user is not a saved beneficiary</div>";
 }
 
 ?>
