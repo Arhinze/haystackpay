374dc875ac5a1a342eb5df12601ca1091a9b97bb
Arinze <francisokoye48@gmail.com>
2025-11-21 08:50:35 +0100

    trying to add AI voice recognition

--- ajax/ai.php
+++ ajax/ai.php
@@ -7,7 +7,7 @@ if (!isset($_GET['message'])) {
 //$style= "position:fixed;z-index:5;background-color:#fff;border-radius:6px;border:1px solid #2b8eeb;height:150px;width:70%;text-align:center;font-weight:bold;top:25%;left:5%";
 
 if ($message = "send 5000 to Frances"){
-    echo "<div style='position:fixed;z-index:5;background-color:#fff;border-radius:6px;border:1px solid #2b8eeb;height:150px;width:70%;text-align:center;font-weight:bold;top:25%;left:5%'>Do you wish to send 5000 Naira to 0095000000 Access Bank - Francis Arinze Okoye ".'<div class="button" onclick=show_div("proceed_to_speech")>'."Yes, Proceed <i class='fa fa-arrow-right'></i></div></div>";
+    echo "<div id='pts' style='position:fixed;z-index:5;background-color:#fff;border-radius:6px;border:1px solid #2b8eeb;height:150px;width:70%;text-align:center;font-weight:bold;top:25%;left:5%'>Do you wish to send 5000 Naira to 0095000000 Access Bank - Francis Arinze Okoye ".'<div class="button" onclick=show_div("pts")>'."Yes, Proceed <i class='fa fa-arrow-right'></i></div></div>";
 } elseif($message = "send 5000 to Francis"){
     echo "<div style='position:fixed;z-index:5;background-color:#fff;border-radius:6px;border:1px solid #2b8eeb;height:150px;width:70%;text-align:center;font-weight:bold;top:25%;left:5%'>Do you wish to send 5000 Naira to 0099000000 Access Bank - Frances Achalugo</div>";
 } elseif ($message = "send 6000 to Cynthia"){
