5e8d5a289e4a2d24d504f932ae7918d7eb189f05
Arinze <francisokoye48@gmail.com>
2025-11-21 07:28:12 +0100

    adding AI voice recognition

--- views/Index_Segments.php
+++ views/Index_Segments.php
@@ -125,7 +125,7 @@ class Index_Segments{
                     </div>
 
                     <div style="display:block;position:fixed;top:15%;left:10%;background-color:#fff;border-radius:6px;border:1px solid #2b8eeb;height:80%;width:70%;text-align:center" id="start_speaking_div">
-                        <div class="features_images_div" style="width:100%;margin:25% 8%">
+                        <div class="features_images_div" style="width:100%;margin:25% 5%">
                             <label for="speak_button"><div class="features_images"><img src="/static/images/speak.png" style="width:60%;height:auto"/></div></label>
                             <div class="features_images_bottom_text">Tap the icon to speak</div>
 
